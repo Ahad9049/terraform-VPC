3af9bcba8ae16eb6bc9804410f2e906355f97607
abdulahad <abdulahad.devops@gmail.com>
2026-06-30 13:29:41 +0500

    dev enviroment complete

---
+++ environment/dev/dev.tfvars
@@ -0,0 +1,9 @@
+aws_region           = "us-east-1"
+vpc_name             = "dev-vpc"
+vpc_cidr             = "10.0.0.0/16"
+public_subnet_cidrs  = ["10.0.3.0/24"]
+private_subnet_cidrs = ["10.0.4.0/24"]
+project              = "vpc-dev"
+owner                = "Dev-Team"
+environment          = "dev"
+
---
+++ environment/dev/variable.tf
@@ -0,0 +1,51 @@
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment (dev, staging, prod)"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "enable_dns_hostnames" {
+  description = "Enable DNS hostnames for the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_support" {
+  description = "Enable DNS support for the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "owner" {
+  description = "Owner of the infrastructure"
+  type        = string
+}
+
+variable "project" {
+  description = "Project name"
+  type        = string
+}
